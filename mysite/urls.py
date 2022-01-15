c987c4aefbd2c4c29a13d2c5e483b1d7334b27fd
pedro <pedro.moisesandrade@gmail.com>
2022-01-15 18:32:08 -0300

    Site maps

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from mysite.blog.sitemaps import PostSitemap
+
+
+sitemaps = {'posts': PostSitemap}
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mysite.blog.urls', namespace='home')),
-    path('blog/', include('mysite.blog.urls', namespace='blog'))
+    path('blog/', include('mysite.blog.urls', namespace='blog')),
+    path(
+        'sitemap.xml', sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'
+    )
 ]
