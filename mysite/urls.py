0eb5092b8bd6a4d57034459993aaa6b7f4ab2399
pedro <pedro.moisesandrade@gmail.com>
2022-01-15 18:05:24 -0300

    Mais comentados

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('mysite.blog.urls', namespace='home')),
     path('blog/', include('mysite.blog.urls', namespace='blog'))
 ]
